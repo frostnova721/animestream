3aebf6b1813fc0d7333d5fcb7753ac177da2b22f
FrostNova <83959988+frostnova721@users.noreply.github.com>
2025-03-06 02:42:24 +0530

    upgrade deps. Compatible with flutter 3.29

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,13 +10,14 @@ import awesome_notifications
 import desktop_webview_window
 import device_info_plus
 import dynamic_color
+import file_picker
 import flutter_secure_storage_macos
 import flutter_web_auth_2
 import open_file_mac
 import package_info_plus
 import path_provider_foundation
 import screen_retriever_macos
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import wakelock_plus
 import window_manager
@@ -28,6 +29,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopWebviewWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewWindowPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
